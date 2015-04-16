d7c0c7fc31ebc2a336811c5695be1f18625f5703
Balraj Brar <balrajb@gmail.com>
2015-04-16 00:39:27 -0700

    CiviCRM extensions with custom extension

---
+++ sites/default/files/civicrm/extensions/org.civicrm.module.civicon/CRM/Civicon/Form/Search/birthday.mgd.php
@@ -0,0 +1,19 @@
+<?php
+// This file declares a managed database record of type "CustomSearch".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Civicon_Form_Search_birthday',
+    'entity' => 'CustomSearch',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'birthday',
+      'description' => 'birthday (org.civicrm.module.civicon)',
+      'class_name' => 'CRM_Civicon_Form_Search_birthday',
+    ),
+  ),
+);
---
+++ sites/default/files/civicrm/extensions/org.civicrm.module.civicon/templates/CRM/Contact/form/Contact.extra.tpl
@@ -0,0 +1,7 @@
+{literal}
+<script type="text/javascript">
+  cj(".custom_field-row.custom_1_-1-row").hide();
+  alert('khjjgh');
+</script>
+
+{/literal}
